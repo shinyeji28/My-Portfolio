428a6d38c35f8d705d30758235cdaeaf826a5cc5
shinyeji28 <xntm10@naver.com>
2022-10-21 17:09:35 +0900

    separating components into study folder

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Accommodate from './components/Accommodate';
-import ProfileCard from './components/ProfileCard';
-import DarkOrLight from './components/DarkOrLight';
-import Blocks from './components/Blocks';
+import Accommodate from './components/study/Accommodate';
+import ProfileCard from './components/study/ProfileCard';
+import DarkOrLight from './components/study/DarkOrLight';
+import Blocks from './components/study/Blocks';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
