f7b5840c2c3f4dd9aad9e504452fc57c635f129d
shinyeji28 <xntm10@naver.com>
2022-12-05 17:04:12 +0900

    create project card list

--- src/components/elements/Project-card.jsx
+++ src/components/elements/Project-card.jsx
@@ -2,14 +2,18 @@ import React from "react";
 import styles from "../css/Elements.module.css";
 
 function Project(props){
+
     return (
         <div className={styles.card_wrap}>
-            {props.data.title}
-            <img src={process.env.PUBLIC_URL + 'images/'+props.data.img} />
-            {props.data.role}
-            {props.data.period}
-            {props.data.summary}
-
+            <div className={styles.logo}>
+                <img src={process.env.PUBLIC_URL + 'images/'+props.data.img} />
+            </div>
+            <div className={styles.text}>
+                <p>{props.data.title}</p>
+                <p>{props.data.role}</p>
+                <p>{props.data.period}</p>
+                <p>{props.data.summary}</p>
+            </div>
         </div>
     );
 }
--- src/components/page/contents/Project.jsx
+++ src/components/page/contents/Project.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import ProjectCard from "../../elements/Project-card.jsx";
-import styles from "../../css/Elements.module.css";
+import styles from "../../css/Main.module.css";
 import jsonData from "../../../assets/json/project-card.json"
 
 function Project(){
+    const section_style={
+        background: "#ffb4b9",
+        height:"700px",
+    };
     const _data=jsonData;
     return (
-        <div>
-            안녕하세요 Project section 입니다.
-            <div className={styles.card_container}>
+        <div className={styles.section_layout} style={section_style}>
+            <p className={styles.title}>웹 개발 프로젝트를 소개합니다.</p>
+            <div className={[styles.section_layout, styles.card_container].join(' ')}>
                 {_data.map(data => { 
                     return <ProjectCard key={data.id} data={data}></ProjectCard>;
                 })}
